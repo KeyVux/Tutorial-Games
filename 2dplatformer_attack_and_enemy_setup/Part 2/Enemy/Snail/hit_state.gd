859c032c27960d3bc85ec869a2134d099d4779ba
KeyVux <99638998+KeyVux@users.noreply.github.com>
2025-01-12 22:28:43 +0700

    Enhance enemy and player interactions with collision layers and state management
    - Added collision layers for player and enemy entities to improve interaction handling.
    - Updated Snail enemy's hit state to include knockback mechanics and timer for state transitions.
    - Refined player movement logic to respect state machine conditions, ensuring smoother gameplay.
    - Integrated new properties in the Snail's scene for better state management and collision detection.

--- 2dplatformer_attack_and_enemy_setup/Part 2/Enemy/Snail/hit_state.gd
+++ 2dplatformer_attack_and_enemy_setup/Part 2/Enemy/Snail/hit_state.gd
@@ -1,12 +1,19 @@
 class_name HitState
 extends State
+@onready var timer: Timer = $Timer
 
 @export var damagetable: Damagetable 
 @export var deadState: State 
 @export var deadAnimationNode: String = "dead"
+@export var knockback_velocity: Vector2 = Vector2(100, 0)
+@export var returnState: State
 
 func _ready():
 	damagetable.connect("onHit", onDamagetableHit)
+
+func on_enter():
+	character.velocity = knockback_velocity
+	timer.start()
 	
 func onDamagetableHit(node: Node, damageAmount: float):
 	if damagetable.Health > 0:
@@ -14,3 +21,10 @@ func onDamagetableHit(node: Node, damageAmount: float):
 	else:
 		emit_signal("interruptState", deadState)
 		playback.travel(deadAnimationNode)
+		
+func onExit():
+	character.velocity = Vector2.ZERO
+
+
+func _on_timer_timeout() -> void:
+	next_state = returnState # Replace with function body.
--- 2dplatformer_attack_and_enemy_setup/Part 2/Enemy/Snail/snail.gd
+++ 2dplatformer_attack_and_enemy_setup/Part 2/Enemy/Snail/snail.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
 @onready var animationTree: AnimationTree = $AnimationTree
+@onready var stateMachine: CharacterStateMachine = $CharacterStateMachine
 
+@export var hitState: State
+@export var movementSpeed: float = 30.0
 @export var startingMoveDirection: Vector2 = Vector2.LEFT
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -18,9 +21,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: Vector2 = startingMoveDirection
-	if direction:
-		velocity.x = direction.x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction && stateMachine.check_if_can_move():
+		velocity.x = direction.x * movementSpeed
+	elif stateMachine.current_state != hitState:
+		velocity.x = move_toward(velocity.x, 0, movementSpeed)
 
 	move_and_slide()
