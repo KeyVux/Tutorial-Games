92ce01cfa857fdb456718c75b1db39142bbbafbc
KeyVux <99638998+KeyVux@users.noreply.github.com>
2025-01-12 20:02:20 +0700

    Enhance player attack mechanics and animation integration
    - Implemented attack state logic in attack.gd, allowing for timed transitions between attack animations.
    - Updated player.gd to correct animation parameter casing for consistency.
    - Refactored player.tscn to streamline attack animations and state transitions, ensuring proper integration with the character state machine.

--- 2dplatformer_attack_and_enemy_setup/Part 2/Character/attack.gd
+++ 2dplatformer_attack_and_enemy_setup/Part 2/Character/attack.gd
@@ -1,1 +1,27 @@
 extends State
+
+@onready var timer: Timer = $Timer
+
+@export var returnState: State
+@export var returnAnimationNode: String = "move"
+@export var attack1Name: String = "attack1"
+@export var attack2Name: String = "attack2"
+@export var attack2_node: String = "attack2"
+
+func state_input(event: InputEvent):
+	if Input.is_action_just_pressed("attack"):
+		timer.start()
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == attack1Name :
+		if timer.is_stopped():
+			next_state = returnState
+			playback.travel(returnAnimationNode)
+			
+		else:
+			playback.travel(attack2_node)
+			
+	if anim_name == attack2Name:
+		next_state = returnState
+		playback.travel(returnAnimationNode)
+		
